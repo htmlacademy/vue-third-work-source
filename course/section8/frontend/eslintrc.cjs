37a1c76330989087bc444b327f0b1cdec0d580a4
Manu Ustenko <ustenko.mi@gmail.com>
2022-08-06 21:34:01 +0200

    Use vue3:recommended

--- course/section8/frontend/eslintrc.cjs
+++ course/section8/frontend/eslintrc.cjs
@@ -4,7 +4,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/test-composable-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
   ],
