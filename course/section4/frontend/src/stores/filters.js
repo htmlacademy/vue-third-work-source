63c3f3926dfdbf92a1990a58e03959628b5a6f1a
Manu Ustenko <ustenko.mi@gmail.com>
2022-09-18 16:43:59 +0200

    Create tasks getters and actions

--- course/section4/frontend/src/stores/filters.js
+++ course/section4/frontend/src/stores/filters.js
@@ -9,5 +9,18 @@ export const useFiltersStore = defineStore('filters', {
 		},
 	}),
 	getters: {},
-	actions: {},
+	actions: {
+		applyFilters ({ item, entity }) {
+			if (!Array.isArray(state.filters[entity])) {
+				state.filters[entity] = item
+			} else {
+				const resultValues = [...state.filters[entity]]
+				const itemIndex = resultValues.findIndex(el => el === item)
+				~itemIndex
+					? resultValues.splice(itemIndex, 1)
+					: resultValues.push(item)
+				state.filters[entity] = resultValues
+			}
+		}
+	},
 })
--- course/section4/frontend/src/stores/tasks.js
+++ course/section4/frontend/src/stores/tasks.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 import tasks from '../mocks/tasks.json'
 import { normalizeTask } from '../common/helpers'
 import { useFiltersStore } from './filters'
+import { useUsersStore } from '@/stores/users'
 
 export const useTasksStore = defineStore('tasks', {
 	state: () => ({
@@ -43,6 +44,10 @@ export const useTasksStore = defineStore('tasks', {
 					.every(([key, callback]) =>
 						!filtersStore.filters[key].length || callback(task))
 			})
+		},
+		getTaskUserById: () => id => {
+			const usersStore = useUsersStore()
+			return usersStore.users.find(user => user.id === id)
 		}
 	},
 	actions: {
@@ -50,5 +55,45 @@ export const useTasksStore = defineStore('tasks', {
 			// Получение данных из json файла будет заменено в последующих разделах
 			this.tasks = tasks.map(task => normalizeTask(task))
 		},
+		updateTasks (tasksToUpdate) {
+			tasksToUpdate.forEach(task => {
+				const index = this.tasks.findIndex(({ id }) => id === task.id)
+				// findIndex вернет элемент массива или -1
+				// Используем bitwise not для определения если index === -1
+				// ~-1 вернет 0, а значит false
+				if (~index) {
+					this.tasks.splice(index, 1, task)
+				}
+			})
+		},
+		addTask (task) {
+			// Нормализуем задачу
+			const newTask = normalizeTask(task)
+			// Добавляем идентификатор, последний элемент в списке задач
+			// После подключения сервера идентификатор будет присваиваться сервером
+			newTask.id = this.tasks.length + 1
+			// Добавляем задачу в конец списка задач в беклоге
+			newTask.sortOrder = this.tasks.filter(task => !task.columnId).length
+			// Если задаче присвоен исполнитель, то добавляем объект юзера в задачу
+			// Это будет добавлено сервером позже
+			if (newTask.userId) {
+				newTask.user = { ...getTaskUserById(newTask.userId) }
+			}
+			// Добавляем задачу в массив
+			this.tasks = [...this.tasks, newTask]
+		},
+		editTask (task) {
+			const index = this.tasks.findIndex(({ id }) => task.id === id)
+			if (~index) {
+				const newTask = normalizeTask(task)
+				if (newTask.userId) {
+					newTask.user = { ...getTaskUserById(newTask.userId) }
+				}
+				this.tasks.splice(index, 1, newTask)
+			}
+		},
+		deleteTask (id) {
+			this.tasks = this.tasks.filter(task => task.id !== id)
+		}
 	},
 })
