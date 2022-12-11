79ca4db5b8a027d15eb52fa08b0e4d2b38c61d4c
Manu Ustenko <ustenko.mi@gmail.com>
2022-12-11 14:35:10 +0100

    Sync with template

--- course/section_7/frontend/vite.config.js
+++ course/section_7/frontend/vite.config.js
@@ -1,27 +1,27 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-	test: {
-		environment: 'jsdom',
-	},
+  test: {
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     host: true,
     port: 8080,
-	  proxy: {
+    proxy: {
       "/api": {
         target: "http://backend:3000/",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  }
-})
+  },
+});
